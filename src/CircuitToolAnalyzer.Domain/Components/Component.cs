9224c5f6bbfec63cfed308579f4b4dd3fe1c879a
JhozzDev <jhossanderhiciano@example.com>
2026-09-05 15:27:55 -0400

    feat: add Component base class and Resistor domain model

---
+++ src/CircuitToolAnalyzer.Domain/Components/Component.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace CircuitToolAnalyzer.Domain.Components
+{
+    public abstract class Component
+    {
+        public Guid Id { get;  }
+        public String Name { get; }
+
+        protected Component(String name)
+        {
+            
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be empty.", nameof(name));
+            }
+
+            Id = Guid.NewGuid();
+            Name = name;
+        }
+    }
+}
---
+++ src/CircuitToolAnalyzer.Domain/Components/Resistor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircuitToolAnalyzer.Domain.Components
+{
+    public class Resistor : Component
+    {
+        public double ResistanceOhms { get; }
+        public Resistor(String name, double resistanceOhms) : base(name)
+        {
+            if (resistanceOhms < 0)
+            {
+                throw new ArgumentException("Resistance must be greater than zero.");
+            }
+            ResistanceOhms = resistanceOhms;
+        }
+    }
+}
