f58b3de927cb866389a6580e0cb4ec279f8d7c05
JhozzDev <jhossanderhiciano@example.com>
2026-09-05 15:35:27 -0400

    feat: add VoltageSource and CurrentSource domain models

---
+++ src/CircuitToolAnalyzer.Domain/Components/CurrentSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircuitToolAnalyzer.Domain.Components
+{
+    public class CurrentSource : Component
+    {
+        public double CurrentAmps { get; }
+
+        public CurrentSource(String name, double currentAmps) : base(name)
+        {
+            if (double.IsNaN(currentAmps) || double.IsInfinity(currentAmps))
+            {
+                throw new ArgumentException("Current must be a valid number.");
+            }
+
+            CurrentAmps = currentAmps;
+        }
+    }
+}
---
+++ src/CircuitToolAnalyzer.Domain/Components/VoltageSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircuitToolAnalyzer.Domain.Components
+{
+    public class VoltageSource : Component
+    {
+        public double VoltageVolts { get; }
+        public VoltageSource(String name, double voltageVolts) : base(name)
+        {
+            if (double.IsNaN(voltageVolts) || double.IsInfinity(voltageVolts))
+            {
+                throw new ArgumentException("Voltage must be a valid number.");
+            }
+            VoltageVolts = voltageVolts;
+        }
+    }
+}
