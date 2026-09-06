ca7f72679c2fcfcec878c5e3016397add165131a
JhozzDev <jhossanderhiciano@example.com>
2026-09-06 14:06:28 -0400

    feat: add AnalysisType enum

---
+++ src/CircuitToolAnalyzer.Domain/Analysis/AnalysisType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircuitToolAnalyzer.Domain.Analysis
+{
+ 
+       public enum AnalysisType
+        {
+            Nodal,
+            Mesh
+        }
+
+}
