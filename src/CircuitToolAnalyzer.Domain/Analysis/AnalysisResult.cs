c3be29cf9d49a0cf87d2fc8445cc321d3e103299
JhozzDev <jhossanderhiciano@example.com>
2026-09-06 14:11:44 -0400

    feat: add AnalysisResult skeleton with AnalysisType

---
+++ src/CircuitToolAnalyzer.Domain/Analysis/AnalysisResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircuitToolAnalyzer.Domain.Analysis
+{
+    public class AnalysisResult
+    {
+        public Guid Id { get; }
+        public  AnalysisType Type { get;  }
+
+
+        public AnalysisResult(AnalysisType type)
+        {
+            Id = Guid.NewGuid();
+            Type = type;
+        }
+    }
+}
