dac335442606dde5cd8f431802aebe2f625c55e4
JhozzDev <jhossanderhiciano@example.com>
2026-09-13 15:46:13 -0400

    feat: add NodeAnalysisSolver to Application

---
+++ src/CircuitToolAnalyzer.Application/Solvers/AnalysisSolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircuitToolAnalyzer.Application.Solvers
+{
+    public class AnalysisSolver
+    {
+
+
+        public double Solve(double voltage, double r1ohms, double r2ohms)
+        {
+
+        if(r1ohms + r2ohms == 0)
+        {
+            throw new ArgumentException("The sum of resistances cannot be zero.");
+        }
+
+            double voltage1 = voltage * (r2ohms/(r1ohms + r2ohms));
+        return voltage1;
+
+        }
+    }
+}
