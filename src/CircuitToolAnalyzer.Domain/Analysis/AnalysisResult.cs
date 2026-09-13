d9b2a7fc3dbde16ea0a1b6d68776cf302b814e2e
JhozzDev <jhossanderhiciano@example.com>
2026-09-13 15:35:03 -0400

    feat: add node voltages to AnalysisResult

--- src/CircuitToolAnalyzer.Domain/Analysis/AnalysisResult.cs
+++ src/CircuitToolAnalyzer.Domain/Analysis/AnalysisResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CircuitToolAnalyzer.Domain.Topology;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,13 +8,24 @@ namespace CircuitToolAnalyzer.Domain.Analysis
     public class AnalysisResult
     {
         public Guid Id { get; }
-        public  AnalysisType Type { get;  }
+
+        private readonly Dictionary<Guid, double> _nodeVoltages;
+
+        public  IReadOnlyDictionary<Guid, double> NodeVoltages => _nodeVoltages.AsReadOnly();
 
 
-        public AnalysisResult(AnalysisType type)
+        public AnalysisType Type { get; }
+
+
+        public AnalysisResult(AnalysisType type, Dictionary<Guid, double> nodeVoltages)
         {
             Id = Guid.NewGuid();
             Type = type;
+            if (nodeVoltages == null)
+            {
+                throw new ArgumentNullException(nameof(nodeVoltages), "Node voltages dictionary cannot be null.");
+            }
+            _nodeVoltages = nodeVoltages;
         }
     }
 }
