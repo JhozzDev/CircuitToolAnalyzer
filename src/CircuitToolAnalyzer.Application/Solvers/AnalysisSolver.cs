b67b37d2d47e62a30bf255b19c9630dbfc4bf338
JhozzDev <jhossanderhiciano@example.com>
2026-09-19 13:54:09 -0400

    feat: generalize nodal solver to work with Circuit domain model

--- src/CircuitToolAnalyzer.Application/Solvers/AnalysisSolver.cs
+++ src/CircuitToolAnalyzer.Application/Solvers/AnalysisSolver.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using CircuitToolAnalyzer.Domain.Analysis;
+using CircuitToolAnalyzer.Domain.Components;
+using CircuitToolAnalyzer.Domain.Topology;
 
 namespace CircuitToolAnalyzer.Application.Solvers
 {
     public class AnalysisSolver
     {
 
+        public Node nodeGND { get; set; }
+
+        public Node nodoNoGND { get; set; }
+
+        public Circuit circuit { get; set; }
 
         public double Solve(double voltage, double r1ohms, double r2ohms)
         {
@@ -19,6 +27,57 @@ namespace CircuitToolAnalyzer.Application.Solvers
             double voltage1 = voltage * (r2ohms/(r1ohms + r2ohms));
         return voltage1;
 
+        }
+
+        public AnalysisResult Solve(Circuit circuit)
+        {
+            var nodeGND = circuit.GetGroundNode();
+
+            var nodoNoGND = circuit.Nodes
+                .First(n => n != nodeGND);
+
+            var voltages = new Dictionary<Guid, double>();
+            voltages[nodeGND.Id] = 0.0;   
+
+            var conexionesDelNodo = circuit.Connections.Where(c => c.NodeA == nodoNoGND || c.NodeB == nodoNoGND);
+
+
+            foreach (var conexion in circuit.Connections)
+            {
+                if (conexion.Component is VoltageSource voltageSource)
+                {
+                    var nodoFuente = conexion.NodeA == nodeGND ? conexion.NodeB : conexion.NodeA;
+                    voltages[nodoFuente.Id] = voltageSource.VoltageVolts;
+                }
+            }
+        
+
+            double sumaConductancias = 0.0;
+            double sumaCorrientesConocidas = 0.0;
+            foreach (var conexion in conexionesDelNodo)
+            {
+
+                if (conexion.Component is Resistor resistor)
+                {
+                    var otroNodo = conexion.NodeA == nodoNoGND ? conexion.NodeB : conexion.NodeA;
+                    double votro = voltages[otroNodo.Id];
+
+                    sumaConductancias += 1.0 / resistor.ResistanceOhms;
+                    sumaCorrientesConocidas += votro / resistor.ResistanceOhms;
+                }
+            }
+
+            double v1 = sumaCorrientesConocidas / sumaConductancias;
+
+            if (!voltages.ContainsKey(nodoNoGND.Id))
+            {
+                voltages[nodoNoGND.Id] = v1;
+            }
+           
+            return new AnalysisResult(
+                AnalysisType.Nodal,
+                voltages
+            );
         }
     }
 }
--- tests/CircuitToolAnalyzer.UnitTests/Solvers/AnalysisSolverTest.cs
+++ tests/CircuitToolAnalyzer.UnitTests/Solvers/AnalysisSolverTest.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using Xunit;
 using CircuitToolAnalyzer.Application.Solvers;
+using CircuitToolAnalyzer.Domain.Topology;
+using CircuitToolAnalyzer.Domain.Components;
 
 
 namespace CircuitToolAnalyzer.UnitTests.Solvers
 {
     public class AnalysisSolverTest
     {
+
+
+
+
+        [Fact]
+        public void Solve_Circuit()
+        {
+            // Arrange
+            var circuit = new Circuit("Test Circuit");
+            var node1 = new Node("Node 1", false);
+            var node2 = new Node("Node GND", true); // Ground node
+            
+            circuit.AddNode(node1);
+            circuit.AddNode(node2);
+            
+            var voltageSource = new VoltageSource("V1", 10.0);
+            var resistor1 = new Resistor("R1", 5.0);
+
+         
+            // Act
+            var connection1 = new Connection(voltageSource, node1, node2);
+            var connection2 = new Connection(resistor1, node1, node2);
+
+            circuit.AddConnection(connection1);
+            circuit.AddConnection(connection2);
+
+            var solver = new AnalysisSolver();
+        
+
+            // Act
+            var result = solver.Solve(circuit);
+            // Assert 
+            Assert.Equal(10.0, result.NodeVoltages[node1.Id]);
+
+        }
+
         [Fact]
         public void Solve_ShouldReturnCorrectVoltage()
         {
