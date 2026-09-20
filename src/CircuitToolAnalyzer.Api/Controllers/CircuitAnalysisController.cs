c2ad8dd9f185b2d89d5b2c8cb5c8343903196a85
JhozzDev <jhossanderh@gmail.com>
2026-09-20 14:04:45 -0400

    feat: add CircuitAnalysisController with JSON enum support

---
+++ src/CircuitToolAnalyzer.Api/Controllers/CircuitAnalysisController.cs
@@ -0,0 +1,14 @@
+﻿using CircuitToolAnalyzer.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CircuitAnalysisController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Analyze([FromBody] CircuitRequestDto dto)
+    {
+        // por ahora, solo para probar que el JSON llega bien
+        return Ok(dto);
+    }
+}
--- src/CircuitToolAnalyzer.Api/Program.cs
+++ src/CircuitToolAnalyzer.Api/Program.cs
@@ -3,8 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 
 var app = builder.Build();
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
