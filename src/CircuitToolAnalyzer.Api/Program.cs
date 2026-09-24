76b1b4397a67b97955617f9b5ce114462cea265f
JhozzDev <jhossanderh@gmail.com>
2026-09-24 18:56:50 -0400

    feat: register EF Core DbContext with SQL Server connection

--- src/CircuitToolAnalyzer.Api/Program.cs
+++ src/CircuitToolAnalyzer.Api/Program.cs
@@ -1,11 +1,15 @@
 using CircuitToolAnalyzer.Application.Builders;
 using CircuitToolAnalyzer.Application.Solvers;
+using Microsoft.EntityFrameworkCore;
+using CircuitToolAnalyzer.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<CircuitBuilder>();
