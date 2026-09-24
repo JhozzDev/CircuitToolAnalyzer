35d8427d2fceb2c3eab71346410d21af5b601bf9
JhozzDev <jhossanderh@gmail.com>
2026-09-24 18:51:43 -0400

    feat: add CircuitDbContext with SavedCircuit DbSet

---
+++ src/CircuitToolAnalyzer.Infrastructure/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CircuitToolAnalyzer.Infrastructure.Entities;
+
+namespace CircuitToolAnalyzer.Infrastructure
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<SavedCircuit> SavedCircuits { get; set; }
+    }
+}
