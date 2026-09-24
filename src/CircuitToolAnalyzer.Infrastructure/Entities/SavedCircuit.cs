8ba186c13a9e294de5e03ce37061d77997866a0a
JhozzDev <jhossanderh@gmail.com>
2026-09-24 18:05:25 -0400

    feat: add SavedCircuit persistence entity

---
+++ src/CircuitToolAnalyzer.Infrastructure/Entities/SavedCircuit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircuitToolAnalyzer.Infrastructure.Entities
+{
+    public class SavedCircuit
+    {
+
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String CircuitJson { get; set; }
+
+        public String ResultJson { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
